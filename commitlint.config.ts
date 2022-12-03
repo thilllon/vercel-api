a742e9a11d4a97c2ece5c91adecb4840da038efb
thilllon <thilllon970@gmail.com>
2022-12-04 02:06:46 +0900

    feat: update package.json

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,6 +2,7 @@ import type { UserConfig } from '@commitlint/types';
 
 const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
+  rules: {},
 };
 
 module.exports = Configuration;
